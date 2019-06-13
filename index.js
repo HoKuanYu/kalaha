c017c2285dea40e1c403f87a2c9e405aec4d3598
HoKuanYu <40305418+HoKuanYu@users.noreply.github.com>
2019-06-13 19:32:49 +0800

    be tiny

--- index.js
+++ index.js
@@ -1,81 +1,52 @@
-var firstPlayer = 6;
-var secondPlayer = 13;
-var firstPlayerTitle;
-var secondPlayerTitle;
-var firstPlayerTurn;
-var secondPlayerTurn;
-var firstPlayerComputer;
-var secondPlayerComputer;
-var firstPlayerDepth;
-var secondPlayerDepth;
+var playerTitle = [];
+var playerComputer = [];
+var playerDepth = [];
+var player;
+
+function ActivePlayerHouse(player) {
+	var playerShop = 6, opponentShop = 13;
+	if (player == 1) {
+		playerShop = 13;
+		opponentShop = 6;
+	}
+
+	for (var i = playerShop - 6; i < playerShop; ++i) {
+		if (document.getElementById("house" + i.toString()).innerHTML != 0)
+			document.getElementById("house" + i.toString()).disabled = false;
+		else
+			document.getElementById("house" + i.toString()).disabled = true;
+	}
+
+	for (var i = opponentShop - 6; i < opponentShop; ++i)
+		document.getElementById("house" + i.toString()).disabled = true;
+}
+
+function ActiveComputerHouse(player) {
+	for (var i = 0; i < 14; ++i) {
+		if (i != 6 && i != 13)
+			document.getElementById("house" + i.toString()).disabled = true;
+	}
+
+	setTimeout(function() {
+		var action = MinMaxDecisionAlphaBetaPurning(playerDepth[player], player);
+		HouseOnClick(action);
+	}, 10);
+}
 
 function HouseButtonActive() {
-	if (firstPlayerTurn && !firstPlayerComputer) {
-		document.getElementById("title").innerHTML = "輪到" + firstPlayerTitle;
-		for (var i = firstPlayer - 6; i < firstPlayer; ++i) {
-			if (document.getElementById("house" + i.toString()).innerHTML != 0)
-				document.getElementById("house" + i.toString()).disabled = false;
-			else
-				document.getElementById("house" + i.toString()).disabled = true;
-		}
+	document.getElementById("title").innerHTML = "輪到" + playerTitle[player];
 
-		for (var i = secondPlayer - 6; i < secondPlayer; ++i)
-			document.getElementById("house" + i.toString()).disabled = true;
-	}
-	else if (firstPlayerTurn && firstPlayerComputer) {
-		console.log('first');
-		document.getElementById("title").innerHTML = "輪到" + firstPlayerTitle;
-
-		for (var i = 0; i < 14; ++i) {
-			if (i != 6 && i != 13)
-				document.getElementById("house" + i.toString()).disabled = true;
-		}
-
-		setTimeout(function() {
-			var action = MinMaxDecision(firstPlayerDepth, firstPlayer);
-			console.log(action);
-			HouseOnClick(action);
-		}, 50);
-	}
-	else if (secondPlayerTurn && !secondPlayerComputer) {
-		document.getElementById("title").innerHTML = "輪到" + secondPlayerTitle;
-		for (var i = firstPlayer - 6; i < firstPlayer; ++i) 
-			document.getElementById("house" + i.toString()).disabled = true;
-
-		for (var i = secondPlayer - 6; i < secondPlayer; ++i){
-			if (document.getElementById("house" + i.toString()).innerHTML != 0)
-				document.getElementById("house" + i.toString()).disabled = false;
-			else
-				document.getElementById("house" + i.toString()).disabled = true;
-		}
-	}
-	else {
-		document.getElementById("title").innerHTML = "輪到" + secondPlayerTitle;
-
-		for (var i = 0; i < 14; ++i) {
-			if (i != 6 && i != 13)
-				document.getElementById("house" + i.toString()).disabled = true;
-		}
-
-		setTimeout(function() {
-			var action = MinMaxDecision(secondPlayerDepth, secondPlayer);
-			HouseOnClick(action);
-		}, 50);
-	}
+	if (playerComputer[player])
+		ActiveComputerHouse(player);
+	else
+		ActivePlayerHouse(player);
 }
 
 function HouseOnClick(pickedHouse) {
-	if (firstPlayerTurn) {
-		document.getElementById("infobox").innerHTML += '<div class="row"><label class="col-lg-5 col-form-label text-lg-right">' + firstPlayerTitle + '選擇了 :</label>'
-											+ '<label class="col-lg-6 col-form-label">' + pickedHouse.toString() + '</label></div>';
-	}
-	else {
-		document.getElementById("infobox").innerHTML += '<div class="row"><label class="col-lg-5 col-form-label text-lg-right">' + secondPlayerTitle + '選擇了 :</label>'
-											+ '<label class="col-lg-6 col-form-label">' + pickedHouse.toString() + '</label></div>';
-	}
+	document.getElementById("infobox").innerHTML += '<div class="row"><label class="col-lg-5 col-form-label text-lg-right">' + playerTitle[player] + '選擇了 :</label>'
+										+ '<label class="col-lg-6 col-form-label">' + pickedHouse.toString() + '</label></div>';
 
 	var house = [];
-
 	for (var i = 0; i < 14; ++i) {
 		house[i] = parseInt(document.getElementById("house" + i.toString()).innerHTML);
 	}
@@ -89,10 +60,8 @@ function HouseOnClick(pickedHouse) {
 		document.getElementById("house" + i.toString()).innerHTML = house[i];
 	}
 
-	if (again == false) {
-		firstPlayerTurn = !firstPlayerTurn;
-		secondPlayerTurn = !secondPlayerTurn;
-	}
+	if (again == false)
+		player = (player == 1 ? 0 : 1);
 
 	if (HasSuccessors(house)) {
 		HouseButtonActive();
@@ -109,14 +78,14 @@ function HouseOnClick(pickedHouse) {
 			if (i != 6 && i != 13)
 				document.getElementById("house" + i.toString()).disabled = true;
 		}
-		if (house[secondPlayer] > house[firstPlayer]) {
-			document.getElementById("title").innerHTML = secondPlayerTitle + "獲勝";
+		if (house[13] > house[6]) {
+			document.getElementById("title").innerHTML = playerTitle[1] + "獲勝";
 		}
-		else if (house[secondPlayer] == house[firstPlayer]) {
+		else if (house[13] == house[6]) {
 			document.getElementById("title").innerHTML = "平手";
 		}
 		else {
-			document.getElementById("title").innerHTML = firstPlayerTitle + "獲勝";
+			document.getElementById("title").innerHTML = playerTitle[0] + "獲勝";
 		}
 	}
 }
@@ -152,19 +121,6 @@ function Relocation(house, pickedHouse) {
 	return false;
 }
 
-function MinMaxDecision(depthMax, player) {
-	var alpha = -1000;
-	var beta = 1000;
-
-	var house = [];
-
-	for (var i = 0; i < 14; ++i) {
-		house[i] = parseInt(document.getElementById("house" + i.toString()).innerHTML);
-	}
-
-	return MaxValue(house, depthMax, 0, alpha, beta, player);
-}
-
 function FinalScoring(house) {
 	for (var i = 0; i < 6; ++i) {
 		house[6] += house[i];
@@ -191,78 +147,6 @@ function HasSuccessors(house) {
 	return player1 && player2;
 }
 
-function MaxValue(house, depthMax, depth, alpha, beta, player) {
-	if (HasSuccessors(house) === false) {
-		FinalScoring(house);
-		return Evaluate(house, player, (player + 7) % 14);
-	}
-	else if (depth >= depthMax) {
-		return Evaluate(house, player, (player + 7) % 14);
-	}
-	else {
-		var action;
-		for (var i = player - 6; i < player; ++i) {
-			if (house[i] === 0)
-				continue;
-
-			var tempHouse = [], tempValue;
-			for (var j = 0; j < 14; ++j)
-				tempHouse[j] = house[j];
-
-			if (Relocation(tempHouse, i))
-				tempValue = MaxValue(tempHouse, depthMax, depth + 2, alpha, beta, player);
-			else
-				tempValue = MinValue(tempHouse, depthMax, depth + 1, alpha, beta, (player + 7) % 14);
-
-			if (alpha < tempValue) {
-				alpha = tempValue;
-				action = i;
-			}
-
-			if (alpha >= beta)
-				break;
-		}
-
-		if (depth === 0)
-			return action;
-		else
-			return alpha;
-	}
-}
-
-function MinValue(house, depthMax, depth, alpha, beta, player) {
-	if (HasSuccessors(house) === false) {
-		FinalScoring(house);
-		return Evaluate(house, (player + 7) % 14, player);
-	}
-	else if (depth >= depthMax) {
-		return Evaluate(house, (player + 7) % 14, player);
-	}
-	else {
-		for (var i = player - 6; i < player; ++i) {
-			if (house[i] === 0)
-				continue;
-
-			var tempHouse = [], tempValue;
-			for (var j = 0; j < 14; ++j)
-				tempHouse[j] = house[j];
-
-			
-			if (Relocation(tempHouse, i))
-				tempValue = MinValue(tempHouse, depthMax, depth + 2, alpha, beta, player);
-			else
-				tempValue = MaxValue(tempHouse, depthMax, depth + 1, alpha, beta, (player + 7) % 14);
-
-			if (beta > tempValue)
-				beta = tempValue;
-
-			if (alpha >= beta)
-				break;
-		}
-		return beta;
-	}
-}
-
 function Start() {
 	for (var i = 0; i < 14; ++i) {
 		if (i != 6 && i != 13)
@@ -271,31 +155,29 @@ function Start() {
 			document.getElementById("house" + i.toString()).innerHTML = 0;
 	}
 
-	firstPlayerTurn = true;
-	secondPlayerTurn = false;
-	firstPlayerTitle = document.getElementById("player1").value;
-	secondPlayerTitle = document.getElementById("player2").value;
+	playerTitle[0] = document.getElementById("player1").value;
+	playerTitle[1] = document.getElementById("player2").value;
+	playerDepth[0] = parseInt(document.getElementById("depth1").value);
+	playerDepth[1] = parseInt(document.getElementById("depth2").value);
+	player = 0;
 
 	if (parseInt(document.getElementById("mode").value) === 0) {
-		firstPlayerComputer = false;
-		secondPlayerComputer = false;
+		playerComputer[0] = false;
+		playerComputer[1] = false;
 	}
 	else if (parseInt(document.getElementById("mode").value.toString()) === 1) {
-		firstPlayerComputer = false;
-		secondPlayerComputer = true;
-		secondPlayerDepth = parseInt(document.getElementById("depth2").value);
+		playerComputer[0] = false;
+		playerComputer[1] = true;
 	}
 	else if (parseInt(document.getElementById("mode").value.toString()) === 2) {
-		firstPlayerComputer = true;
-		secondPlayerComputer = false;
-		firstPlayerDepth = parseInt(document.getElementById("depth1").value);
+		playerComputer[0] = true;
+		playerComputer[1] = false;
 	}
 	else {
-		firstPlayerComputer = true;
-		secondPlayerComputer = true;
-		firstPlayerDepth = document.getElementById("depth1").value;
-		secondPlayerDepth = parseInt(document.getElementById("depth2").value);
+		playerComputer[0] = true;
+		playerComputer[1] = true;
 	}
+
 	document.getElementById("gametable").style.display = "block";
 	document.getElementById("main").style.display = "none";
 
